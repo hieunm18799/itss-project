8922b5f7422649e8312dda98710cfe3504ac9f21
Nguyen Minh Hieu <vampirehutbin@gmail.com>
2021-06-12 02:41:35 +0700

    fix User + feature Grammar

--- src/screens/lessons/grammar/index.js
+++ src/screens/lessons/grammar/index.js
@@ -15,8 +15,15 @@ export const Grammar = ({ doneLesson, dataLesson }) => {
       </div>
       <div className="content container-fluid text-center">
         <h3 className="inline-block mb-3">{dataLesson?.title}</h3>
-        <div className="img-content">
-          <img src={dataLesson?.url} className="" alt={dataLesson?.title} />
+        <div className="grammar-box">
+          <div className="grammar__title">Cách dùng:</div>
+          <p className="grammar__content grammar__structure">{dataLesson?.structure}</p>
+          <div className="grammar__title">Ý nghĩa:</div>
+          <p className="grammar__content">{dataLesson?.mean}</p>
+          <div className="grammar__title">Giải thích:</div>
+          <p className="grammar__content">{dataLesson?.detail}</p>
+          <div className="grammar__title">Ví dụ:</div>
+          <p className="grammar__content">{dataLesson?.example}</p>
         </div>
       </div>
       <div className="done-bar fixed-bottom d-flex justify-content-center">
