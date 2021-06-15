fd4f53789cf952ae8376a6c32ef162f34a4f5f9c
Sakeshioyaki <nguyetanh150299@gmail.com>
2021-06-15 09:34:43 +0700

    update dashboard

--- src/screens/admin/Dashboard/Dashboard.js
+++ src/screens/admin/Dashboard/Dashboard.js
@@ -1,15 +1,44 @@
-import React, { lazy } from 'react'
+import React from 'react';
+import {WidgetsDropdown} from '../../../components/Widgets/WidgetsDropdown.js';
+import {
+  CRow,
+  CCol,
+  CWidgetDropdown,
+} from '@coreui/react'
+import { getStyle } from '@coreui/utils'
+import { CChartLine } from '@coreui/react-chartjs'
+import CIcon from '@coreui/icons-react'
 
-const WidgetsDropdown = lazy(() => import('../../../components/Widgets/WidgetsDropdown'));
-const WidgetsBrand = lazy(() => import('../../../components/Widgets/WidgetsBrand.js'))
 
 const Dashboard = () => {
-  const random = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
 
   return (
-      <div>hiihihihi</div>
+    <CRow>
+    <CCol sm="6" lg="4">
+      <CWidgetDropdown
+        className="mb-4"
+        color="primary"
+        value="9.823"
+        title="Members online"
+      />
+    </CCol>
+    <CCol sm="6" lg="4">
+          <CWidgetDropdown
+          className="mb-4"
+          color="info"
+          value="9.823"
+          title="Members online"
+        />
+    </CCol>
+    <CCol sm="6" lg="4">
+      <CWidgetDropdown
+        className="mb-4"
+        color="warning"
+        value="9.823"
+        title="Members online"
+      />
+    </CCol>
+  </CRow>
   )
 }
 
--- src/screens/admin/Kosu/KosuList.js
+++ src/screens/admin/Kosu/KosuList.js
@@ -1,4 +1,19 @@
 import React, { lazy } from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { DocsCallout, Example } from 'src/reusable'
 
 const WidgetsDropdown = lazy(() => import('../../../components/Widgets/WidgetsDropdown'));
 const WidgetsBrand = lazy(() => import('../../../components/Widgets/WidgetsBrand.js'))
